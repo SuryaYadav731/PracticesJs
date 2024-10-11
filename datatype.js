dd2db11f5a8fdc0c197439389d48a9f5c354cbe6
Surya Yadav <157280253+SuryaYadav731@users.noreply.github.com>
2024-10-11 22:38:40 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,107 @@
+// "use strict"  treat all js code as newer version
+
+// let a = "surya";
+// let b = 0;
+// let c= 10;
+// let d= false;
+// let e;
+
+
+// console.log(typeof a);
+// console.log(typeof b);
+
+// console.log(typeof c);
+
+// console.log(typeof d);
+// console.log(typeof e);
+
+
+// let k = null ;
+// console.log(typeof k)
+
+
+// number , boolean, string, NaN, bigint, Null, symbol-> unique primitive data type
+// object is non-primitive
+// null is object 
+
+// let score = 30;
+// console.log (typeof score);
+
+// let chan = Number(score);
+
+// console.log(typeof score);
+
+// "33"-> 33
+// "33abc"-> NaN
+//true -> 1, false ->0
+
+
+// let s = 1;
+// let r = Boolean(s);
+// console.log(typeof s);
+// console.log(typeof r)
+// console.log(r);
+
+
+//operation
+
+// let a= "surya";
+// let b = " yadav";
+// let c = a+b;
+// console.log(c)
+// console.log("1" + 2 + 2 +3)
+
+// console.log(1 + 2 + "2" +3 + 3)
+
+// console.log(1 + 2 + 2 +"3")
+
+// console.log(1 + "2" + 2 +3)
+
+// console.log( 1+ 2+2 +3)
+
+
+// let a = 2+3-4*5/10;
+// console.log(a);
+
+// let a = 100
+// console.log(a)
+// ++a;
+// console.log(a++)
+// a++;
+
+// console.log(a++)
+
+
+//comparison
+
+// let a ="10";
+// let b = 10;
+
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a==b);
+// console.log(a=b);
+// console.log(a === b);
+// console.log(a>=b);
+
+// console.log("12/10/2024" )
+
+// Primitive datat type
+// 7 type: String, Number, Boolean, null, undefined, Symbol,BigInt
+
+//Non- Primitive Data Type(Refrence Data Type)
+
+// Array, Objects, Functions
+
+// function sum(){
+//   console.log("surya yadav")
+// }
+// sum();
+
+// stack ->primitive , Heap -> Non- Primitive
+
+// const a =" hello";
+// const b = "surya";
+
+// console.log(`${a} ${b}`)
+
---
+++ test.js
@@ -0,0 +1,29 @@
+console.log("surya yadav") 
+let a =10;
+let b = 20;
+let c = a + b;
+console.log(c)
+  c = a-b;
+  console.log(c)
+  c=a*b;
+  console.log(c)
+  c= a/b;
+  console.log(c)
+  c= a%b;
+
+console.log(c)
+
+
+const nam = "surya yadav";
+const vilage = "Chhitaune";
+const education = "B.tech"
+
+console.log(nam);
+console.log(vilage, education);
+
+b = 490;
+console.log(b);
+
+console.table([a,b,c]);
+let z;
+console.log(z);
