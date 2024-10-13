509c28d051eba8e7c90fd33db713de9032b640a2
Surya Yadav <157280253+SuryaYadav731@users.noreply.github.com>
2024-10-13 22:19:55 +0530

    Add files via upload

---
+++ ifelse.js/if.js
@@ -0,0 +1,160 @@
+//console.log("hello")
+
+// let a= 2;
+// let b= 3;
+// let sum = a + b;
+// console.log(sum)
+
+// let a= 10;
+// let b= 5;
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+
+// let name = "this is a cat it is very dangerous";
+// let name2= "the tiger is more dangerous than cat";
+// let combine= name +name2;
+// console.log(combine)
+// console.log(name.length, name2.length)
+// console.log(` the animal is best ${name}`)
+
+// let name = `hello
+// what is your name
+// where are you from`
+
+// console.log(name)
+
+//  let name2 = "welcome ";
+//  let name = "surya yadav";
+//  console.log(name2 +  name)
+//  console.log(`welcome ${name}`)
+
+// let name= "Surya "
+// console.log(typeof name)
+
+// let num = 45
+// console.log(typeof num)
+
+// let num1 = 45
+// let num2 = 44
+// let output= console.log(num1==num2)
+// console.log(typeof output)
+
+
+// let a= 'surya \n'
+
+// let b= "yadav\n"
+// let c=(`welcome ${a} ${b}`)
+// console.log(a, b, c)
+
+// console.log("hello")
+
+// let a = 3;
+// let b = a+3;
+// console.log(b);
+// a += 5;
+// b *= 5;
+// console.log(a)
+// console.log(b)
+
+// let a= 4
+// console.log(a)
+// a++
+// console.log(a)
+
+
+// ++a
+// console.log(a)
+// --a
+// console.log(a)
+// a--
+// console.log(a)
+// console.log(a)
+// const  name = "surya yadav";
+// document.write(name)
+// console.log( typeof (2>3))
+// console.log('true' )
+
+// let age = 17;
+
+// if (age>18){
+//     console.log("you can vote")
+
+// }else{
+//     console.log("you can not vote")
+// }
+
+
+// let age = 20;
+// if(age){
+//     console.log("you have a valid age");
+// }else{
+//     console.log("app hain hi nahi");
+// }
+
+// let age = 12
+// let result = age>18? "aduult" : "kid";
+// console.log(result);
+
+
+// let age ;
+// let FinalAge = age || "30";
+// console.log(FinalAge);
+
+
+//Function
+
+//  function sum(a, b){
+//     return  a+b;
+//  }
+//  console.log(sum(2,3))
+
+
+// function sum(a,b){
+//     let sum = a+b;
+//     return sum;
+// }
+// console.log(sum(9,3));
+// function check(num){
+//     let rem = num%2;
+//     let check = rem==0
+//     return check;
+// }
+// console.log(check(4));
+
+
+
+// function larger(num1, num2){
+//     if(num1>num2){
+//         return num1;
+//     }else{
+//         return num2;
+//     }
+// }console.log(larger(6, 5))
+
+// let student = {
+//     name:"surya",
+//     last: "yadav",
+//     vill: "chhitauna"
+// }
+// console.log(student.name);
+
+
+//  let array = [ 'ram', 29, 86];
+
+//  array.push('4');
+//  console.log(array);
+//  array.pop();
+//  console.log(array);
+//  array.shift();
+//  console.log(array);
+//  array.unshift("hello");
+//  console.log(array);
+
+ 
+
+
+
